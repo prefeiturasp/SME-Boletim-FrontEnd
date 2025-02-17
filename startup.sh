9480b8809b51a871f405e57852117b03106acdb0
Felipe Piorini <felipemolica@hotmail.com>
2025-02-17 10:33:36 -0300

    fix: Altera nome da env

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ for file in $files
 do
   cp $file /tmp/$file
   rm $file
-  envsubst '${VITE_SME_SERAP_ACOMPANHAMENTO_API},${VITE_SME_SERAP},${VITE_CHAVE_API}' < /tmp/$file > $file
+  envsubst '${VITE_SME_SERAP_BOLETIM_API},${VITE_SME_SERAP},${VITE_CHAVE_API}' < /tmp/$file > $file
 done
 
 nginx -g 'daemon off;'
