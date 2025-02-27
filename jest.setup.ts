5a58010760e578e2d1dc376a979fa1936a1ddadf
Felipe Piorini <felipemolica@hotmail.com>
2025-02-27 14:55:21 -0300

    feat: Adiciona novos testes ao sistema

--- jest.setup.ts
+++ jest.setup.ts
@@ -8,3 +8,26 @@ global.matchMedia = jest.fn().mockImplementation((query) => ({
 }));
 
 process.env.VITE_API_URL = "http://localhost:3000";
+
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+import { TextEncoder, TextDecoder } from "util";
+
+global.TextEncoder = TextEncoder;
+
+beforeAll(() => {
+  Object.defineProperty(global, "import", {
+    value: {
+      meta: {
+        env: {
+          VITE_SERAP_URL: "/",
+        },
+      },
+    },
+    writable: true,
+  });
+});
--- src/componentes/escolherEscola/escolherEscola.test.js
+++ src/componentes/escolherEscola/escolherEscola.test.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import EscolherEscola from "./EscolherEscola";
-jest.mock("../../__mocks__/servicos");
+import { servicos } from "../../__mocks__/servicos";
+
+jest.mock("../../servicos");
 
 const mockStore = configureStore([]);
 
@@ -12,19 +14,41 @@ describe("EscolherEscola Componente", () => {
 
   beforeEach(() => {
     store = mockStore({
-      escola: { escolaSelecionada: "" },
+      escola: { escolaSelecionada: { ueId: null, descricao: "" } },
     });
+
+    servicos.get.mockResolvedValueOnce([
+      { ueId: "1", descricao: "Escola A" },
+      { ueId: "2", descricao: "Escola B" },
+    ]);
   });
 
-  test("Renderizar componente EscolherEscola ", () => {
+  test("Renderizar componente EscolherEscola", async () => {
     render(
       <Provider store={store}>
         <EscolherEscola />
       </Provider>
     );
-    expect(
-      screen.getByText("EMEF Bartolomeu Lourenço de Gusmão")
-    ).toBeInTheDocument();
-    expect(screen.getByText("Filtrar")).toBeInTheDocument();
+
+    screen.debug();
+
+    await waitFor(() =>
+      expect(screen.getByRole("combobox")).toBeInTheDocument()
+    );
+  });
+
+  test("Abrir drawer de filtros", async () => {
+    render(
+      <Provider store={store}>
+        <EscolherEscola />
+      </Provider>
+    );
+
+    const botaoFiltrar = screen.getByText("Filtrar");
+    fireEvent.click(botaoFiltrar);
+
+    await waitFor(() => {
+      expect(screen.getByText("Níveis")).toBeInTheDocument();
+    });
   });
 });
