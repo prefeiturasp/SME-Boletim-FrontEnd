e749ad019f583a6f6a94a94927a3a13812f8002c
Felipe Piorini <felipemolica@hotmail.com>
2025-02-28 09:08:32 -0300

    fix: Altera ordem do script

--- startup.sh
+++ startup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+echo "VITE_API_URL=${VITE_API_URL}"
+echo "VITE_BOLETIM_VERSAO=${VITE_BOLETIM_VERSAO}"
+echo "VITE_SERAP_URL=${VITE_SERAP_URL}"
+
 cd /usr/share/nginx/html/assets
-files=$(ls)
-for file in $files
-do
-  cp $file /tmp/$file
-  rm $file
-  envsubst '${VITE_API_URL},${VITE_BOLETIM_VERSAO},${VITE_SERAP_URL}' < /tmp/$file > $file
+for file in $(ls); do
+  cp "$file" "/tmp/$file"
+  envsubst < "/tmp/$file" > "$file"
+  rm "/tmp/$file"
 done
 
 nginx -g 'daemon off;'
