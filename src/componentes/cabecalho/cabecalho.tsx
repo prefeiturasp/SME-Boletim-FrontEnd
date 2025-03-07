d53a0df64bb36f4607bb221286e468f3bafceacc
Felipe Piorini <felipemolica@hotmail.com>
2025-03-07 11:16:35 -0300

    fix: Remove temporariamente a env de versao e url do serap

--- src/componentes/cabecalho/cabecalho.tsx
+++ src/componentes/cabecalho/cabecalho.tsx
@@ -6,7 +6,7 @@ import { Row } from "antd";
 const { Header } = Layout;
 import { Link } from "react-router-dom";
 
-const linkRetorno = import.meta.env.VITE_SERAP_URL || "/";
+const linkRetorno = "https://hom-serap.sme.prefeitura.sp.gov.br/";
 
 const Cabecalho: React.FC = () => {
   return (
