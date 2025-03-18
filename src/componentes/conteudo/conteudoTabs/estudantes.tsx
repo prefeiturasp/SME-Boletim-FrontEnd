6405c08cc00b1dfb51d3ed26363ae4c1d40b101f
Felipe Piorini <felipemolica@hotmail.com>
2025-03-18 15:27:13 -0300

    feat: Adiciona aba estudantes com o datatables

--- src/componentes/conteudo/conteudoTabs/estudantes.tsx
+++ src/componentes/conteudo/conteudoTabs/estudantes.tsx
@@ -1,64 +1,128 @@
-import React from "react";
-import { json2csv } from "json-2-csv";
+import React, { useEffect, useState } from "react";
+import { Table, Spin } from "antd";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
+import { servicos } from "../../../servicos";
+import "./estudantes.css";
 
 const Estudantes: React.FC = () => {
-  const data = [
-    {
-      turma: "5A",
-      abaixoBasico: "2 (40%)",
-      basico: "2 (40%)",
-      adequado: "1 (20%)",
-      avancado: "0 (0%)",
-      total: 5,
-      mediaProficiencia: 191.19,
-    },
-    {
-      turma: "5B",
-      abaixoBasico: "2 (66.67%)",
-      basico: "1 (33.33%)",
-      adequado: "0 (0%)",
-      avancado: "0 (0%)",
-      total: 3,
-      mediaProficiencia: 155.85,
-    },
-    {
-      turma: "6A",
-      abaixoBasico: "0 (0%)",
-      basico: "0 (0%)",
-      adequado: "0 (0%)",
-      avancado: "25 (100%)",
-      total: 25,
-      mediaProficiencia: 163.04,
-    },
-    {
-      turma: "6B",
-      abaixoBasico: "0 (0%)",
-      basico: "0 (0%)",
-      adequado: "0 (0%)",
-      avancado: "25 (100%)",
-      total: 25,
-      mediaProficiencia: 157.57,
-    },
-  ];
+  const [dados, setDados] = useState<any[]>([]);
+  const [disciplinas, setDadosDisciplinas] = useState<any[]>([]);
 
-  const exportToCSV = async () => {
+  const [carregando, setCarregando] = useState(false);
+  const [pagina, setPagina] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalRegistros, setTotalRegistros] = useState(0);
+
+  const escolaSelecionada = useSelector(
+    (state: RootState) => state.escola.escolaSelecionada
+  );
+
+  const buscarDadosEstudantes = async (paginaAtual = 1, tamanhoPagina = 10) => {
     try {
-      const csv = await json2csv(data);
-
-      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "dados.csv");
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+      setCarregando(true);
+      const resposta = await servicos.get(
+        `/api/boletimescolar/095532/estudantes?pageNumber=${paginaAtual}&pageSize=${tamanhoPagina}`
+      );
+      setDados(resposta.estudantes.itens || []);
+      setDadosDisciplinas(resposta.disciplinas || []);
+      setTotalRegistros(resposta.estudantes.totalRegistros || 0);
     } catch (error) {
-      console.error("Erro ao exportar CSV:", error);
+      console.error("Erro ao buscar os dados da tabela:", error);
+    } finally {
+      setCarregando(false);
     }
   };
 
-  return <button onClick={exportToCSV}>Exportar CSV</button>;
+  useEffect(() => {
+    if (escolaSelecionada) {
+      buscarDadosEstudantes(pagina, pageSize);
+    }
+  }, [escolaSelecionada, pagina, pageSize]);
+
+  const getNivelColor = (nivel: string) => {
+    switch (nivel) {
+      case "Abaixo do básico":
+        return "#FF5959";
+      case "Básico":
+        return "#FEDE99";
+      case "Avançado":
+        return "#99FF99";
+      case "Adequado":
+        return "#9999FF";
+      default:
+        return "black";
+    }
+  };
+
+  const colunas = [
+    {
+      title: "Componente curricular",
+      dataIndex: "disciplina",
+      key: "disciplina",
+    },
+    { title: "Ano", dataIndex: "anoEscolar", key: "anoEscolar" },
+    { title: "Turma", dataIndex: "turma", key: "turma" },
+    { title: "EOL do estudante", dataIndex: "alunoRa", key: "alunoRa" },
+    { title: "Nome do estudante", dataIndex: "alunoNome", key: "alunoNome" },
+    { title: "Proficiência", dataIndex: "proficiencia", key: "proficiencia" },
+    {
+      title: "Nível",
+      dataIndex: "nivelDescricao",
+      key: "nivelDescricao",
+      render: (text: string) => (
+        <span style={{ color: getNivelColor(text) }}>{text}</span>
+      ),
+    },
+  ];
+
+  return (
+    <Spin spinning={carregando} tip="Carregando...">
+      <div>
+        <p className="secao-sobre-estudantes">
+          Esta seção apresenta uma tabela e gráficos que ilustram a proficiência
+          de cada aluno, separado por turmas.
+        </p>
+
+        <p className="secao-titulo-estudantes">
+          Distribuição de estudantes em cada nível por turma.
+        </p>
+
+        <p className="secao-titulo-disciplinas">
+          {disciplinas.length > 1
+            ? `${disciplinas.slice(0, -1).join(", ")} e ${
+                disciplinas[disciplinas.length - 1]
+              }`
+            : disciplinas[0]}
+        </p>
+
+        <Table
+          columns={colunas}
+          dataSource={dados}
+          rowKey="alunoRa"
+          locale={{ emptyText: "Não encontramos dados" }}
+          pagination={{
+            current: pagina,
+            total: totalRegistros,
+            pageSize,
+            showSizeChanger: true,
+            showTotal: (total, range) =>
+              `${range[0]}-${range[1]} de ${total} alunos`,
+            pageSizeOptions: ["10", "20", "30", "40", "50", "100"],
+            onShowSizeChange: (_, newSize) => setPageSize(newSize),
+            onChange: (page, newSize) => {
+              setPagina(page);
+              setPageSize(newSize);
+            },
+            position: ["bottomCenter"],
+            locale: {
+              items_per_page: "/ página",
+            },
+          }}
+        />
+      </div>
+    </Spin>
+  );
 };
 
 export default Estudantes;
