211f90658bd43116c29fde272cca3fcf5d6f2253
Lincoln <link7-7@hotmail.com>
2025-09-18 11:39:20 -0300

    espaço em branco

--- src/componentes/tabela/tabelaComparativa/tabelaComparativa.tsx
+++ src/componentes/tabela/tabelaComparativa/tabelaComparativa.tsx
@@ -3,23 +3,22 @@ import "./tabelaComparativa.css";
 import { Card, Table, Progress } from "antd";
 import dreComparativa from "../../../mocks/dreComparativa.json"
 
-
 interface AplicacaoItem {
-  descricao: string;
-  mes: string;
-  valorProficiencia: number;
-  nivelProficiencia: string;
-  ["Qtde UE"]: string;
-  ["Qtde Estudante"]: string;
+    descricao: string;
+    mes: string;
+    valorProficiencia: number;
+    nivelProficiencia: string;
+    ["Qtde UE"]: string;
+    ["Qtde Estudante"]: string;
 }
 
 interface Payload {
-  variacao: number;
-  Aplicacao: AplicacaoItem[];
+    variacao: number;
+    Aplicacao: AplicacaoItem[];
 }
 
-const TabelaComparativa: React.FC<TabelaComparativaProps> = ({ 
-    aplicacaoSelecionada, componenteSelecionado, anoSelecionado 
+const TabelaComparativa: React.FC<TabelaComparativaProps> = ({
+    aplicacaoSelecionada, componenteSelecionado, anoSelecionado
 }) => {
 
     // 🔹 Linhas fixas
@@ -37,45 +36,45 @@ const TabelaComparativa: React.FC<TabelaComparativaProps> = ({
             dataIndex: colKey,
             key: colKey,
             align: "left" as const,
-            width: 120,            
+            width: 120,
             onHeaderCell: () => ({
                 className: "tabela-comparativa-header", // 🔹 aplica sua classe
             }),
         };
     });
 
-   // 🔹 Monta o dataSource transposto
+    // 🔹 Monta o dataSource transposto
     const dataSource = linhasFixas.map((linha) => {
         const row: any = { key: linha.key, aplicacao: linha.aplicacao };
 
         dreComparativa.Aplicacao.forEach((item, index) => {
-        const colKey = `${item.descricao}-${item.mes || index}`;
+            const colKey = `${item.descricao}-${item.mes || index}`;
 
-        if (linha.key === "proficiencia") {
-            row[colKey] = (
-            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "flex-start" }}>
-                <div>{item.valorProficiencia}</div>
-                <Progress
-                percent={Math.min(100, (item.valorProficiencia / 300) * 100)} // Exemplo de cálculo
-                size="small"
-                showInfo={false}
-                strokeWidth={9}
-                strokeColor={
-                    item.nivelProficiencia === "Adequado"
-                    ? "green"
-                    : item.nivelProficiencia === "Básico"
-                    ? "orange"
-                    : "red"
-                }
-                style={{ width: "100%", paddingLeft: 8 }}
-                />                
-            </div>
-            );
-        } else if (linha.key === "qtdeUE") {
-            row[colKey] = item["Qtde UE"] || "-";
-        } else if (linha.key === "qtdeEstudante") {
-            row[colKey] = item["Qtde Estudante"] || "-";
-        }
+            if (linha.key === "proficiencia") {
+                row[colKey] = (
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "flex-start" }}>
+                        <div>{item.valorProficiencia}</div>
+                        <Progress
+                            percent={Math.min(100, (item.valorProficiencia / 300) * 100)} // Exemplo de cálculo
+                            size="small"
+                            showInfo={false}
+                            strokeWidth={9}
+                            strokeColor={
+                                item.nivelProficiencia === "Adequado"
+                                    ? "green"
+                                    : item.nivelProficiencia === "Básico"
+                                        ? "orange"
+                                        : "red"
+                            }
+                            style={{ width: "100%", paddingLeft: 8 }}
+                        />
+                    </div>
+                );
+            } else if (linha.key === "qtdeUE") {
+                row[colKey] = item["Qtde UE"] || "-";
+            } else if (linha.key === "qtdeEstudante") {
+                row[colKey] = item["Qtde Estudante"] || "-";
+            }
         });
 
         return row;
@@ -94,46 +93,46 @@ const TabelaComparativa: React.FC<TabelaComparativaProps> = ({
                 className: "tabela-comparativa-header",
             }),
             render: (text: string) => (
-            <div style={{color: "#595959", fontWeight: 700, whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>
-                {text}
-            </div>
+                <div style={{ color: "#595959", fontWeight: 700, whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>
+                    {text}
+                </div>
             ),
         },
         ...colunasDinamicas,
     ];
-  
 
-  return (
-    <>
-     <div className="tabela-comparativa-titulo">
-        <b>Tabela comparativa</b>
-     </div>
-     <div className="tabela-comparativa-descricao">
-        <div className="tabela-comparativa-descricao-texto">
-            A tabela exibe a proficiência da Secretaria Municipal de Educação nas aplicações da Prova São Paulo e da Prova Saberes e Aprendizagens.
-        </div>
-        <div className="tabela-comparativa-labels">
-            <div className="tabela-comparativa-labels-item" style={{ width: "130px" }}>{componenteSelecionado?.label}</div>
-            <div className="tabela-comparativa-labels-item" style={{ width: "38px" }}>{anoSelecionado?.label}</div>
-            <div className="tabela-comparativa-labels-item" style={{ width: "31px" }}>{aplicacaoSelecionada?.label}</div>            
-        </div>
-     </div>
-     <br />    
-        <Card className="tabela-comparativa-variacao-card">
-            <div className="tabela-comparativa-variacao">
-                <div className="tabela-comparativa-variacao-label">Variação</div>
-                <div className="tabela-comparativa-variacao-valor">{dreComparativa.variacao > 0 ? `+${dreComparativa.variacao}%` : `${dreComparativa.variacao}%`}</div>
-            </div>            
-            <Table
-            columns={columns}
-            dataSource={dataSource}
-            pagination={false}
-            bordered
-            scroll={{ x: true }}
-            />
-        </Card>
-    </>
-  );
+
+    return (
+        <>
+            <div className="tabela-comparativa-titulo">
+                <b>Tabela comparativa</b>
+            </div>
+            <div className="tabela-comparativa-descricao">
+                <div className="tabela-comparativa-descricao-texto">
+                    A tabela exibe a proficiência da Secretaria Municipal de Educação nas aplicações da Prova São Paulo e da Prova Saberes e Aprendizagens.
+                </div>
+                <div className="tabela-comparativa-labels">
+                    <div className="tabela-comparativa-labels-item" style={{ width: "130px" }}>{componenteSelecionado?.label}</div>
+                    <div className="tabela-comparativa-labels-item" style={{ width: "38px" }}>{anoSelecionado?.label}</div>
+                    <div className="tabela-comparativa-labels-item" style={{ width: "31px" }}>{aplicacaoSelecionada?.label}</div>
+                </div>
+            </div>
+            <br />
+            <Card className="tabela-comparativa-variacao-card">
+                <div className="tabela-comparativa-variacao">
+                    <div className="tabela-comparativa-variacao-label">Variação</div>
+                    <div className="tabela-comparativa-variacao-valor">{dreComparativa.variacao > 0 ? `+${dreComparativa.variacao}%` : `${dreComparativa.variacao}%`}</div>
+                </div>
+                <Table
+                    columns={columns}
+                    dataSource={dataSource}
+                    pagination={false}
+                    bordered
+                    scroll={{ x: true }}
+                />
+            </Card>
+        </>
+    );
 };
 
 export default TabelaComparativa;
